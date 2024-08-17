f6e6a308b06c9765c92ee408a6ffdb80aaa211e5
griguk77 <griguk77@gmail.com>
2024-08-17 18:53:48 +0300

    Initial commit

---
+++ src/main/java/api/ReqresTest.java
@@ -0,0 +1,34 @@
+package api;
+
+import io.restassured.http.ContentType;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.given;
+
+public class ReqresTest {
+
+    private final static String URL = "https://reqres.in/";
+
+    @Test
+    public void checkAvatarAndIdTest() {
+        Specifications.installSpecifications(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+        List<UserData> users = given()
+                .when()
+                .get("api/users?page=2")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", UserData.class);
+        users.forEach(x -> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
+        Assert.assertTrue(users.stream().allMatch(x -> x.getEmail().endsWith("@reqres.in")));
+
+        List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
+        List<String> ids = users.stream().map(x -> x.getId().toString()).collect(Collectors.toList());
+        for (int i = 0; i < avatars.size(); i++) {
+            Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
+        }
+
+    }
+}
