088d7fc912221b9dec793de713040a89cb267397
griguk77 <griguk77@gmail.com>
2024-08-18 14:47:03 +0300

    Add sorted years post test

--- src/main/java/api/ReqresTest.java
+++ src/main/java/api/ReqresTest.java
@@ -51,4 +51,33 @@ public class ReqresTest {
         Assert.assertEquals(successReg.getId(), id);
         Assert.assertEquals(successReg.getToken(), token);
     }
+
+    @Test
+    public void unSuccessRegTest() {
+        Specifications.installSpecifications(Specifications.requestSpec(URL), Specifications.responseSpecError400());
+        String error = "Missing password";
+        Register user = new Register("sydney@fife", "");
+        UnSuccessReg unSuccessReg = given()
+                .body(user)
+                .when()
+                .post("api/register")
+                .then().log().all()
+                .extract().as(UnSuccessReg.class);
+        Assert.assertNotNull(unSuccessReg.getError());
+
+        Assert.assertEquals(unSuccessReg.getError(), error);
+    }
+
+    @Test
+    public void sortedYearsTest() {
+        Specifications.installSpecifications(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+        List<ColorsData> colors = given()
+                .when()
+                .get("api/unknown")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", ColorsData.class);
+        List<Integer> years = colors.stream().map(ColorsData::getYear).collect(Collectors.toList());
+        List<Integer> sortedYears = years.stream().sorted().collect(Collectors.toList());
+        Assert.assertEquals(sortedYears, years);
+    }
 }
---
+++ src/main/java/api/UnSuccessReg.java
@@ -0,0 +1,17 @@
+package api;
+
+public class UnSuccessReg {
+    private String error;
+
+    public UnSuccessReg() {
+        super();
+    }
+
+    public UnSuccessReg(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
